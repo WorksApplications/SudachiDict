ce53a22d59eebdc82dfd9f348db77e43068cffcc
hiroshi-matsuda-rit <40782025+hiroshi-matsuda-rit@users.noreply.github.com>
2019-07-06 13:56:03 +0900

    add package_python.sh and setup.py.template

---
+++ package_python.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+version=`grep -oP -m 1 '<version>\K([^<]+)' pom.xml`
+
+set +e
+rm -r target/python/${version}
+
+set -e
+home=`pwd`
+for dic_type in small core full
+do
+    temp=target/python/${version}/${dic_type}
+    pkg=${temp}/sudachidict_${dic_type}
+    mkdir -p ${pkg}/resources/
+    touch ${pkg}/__init__.py
+    cp target/system_${dic_type}.dic ${pkg}/resources/system.dic
+    cp README.md ${temp}
+    cat setup.py.template | sed "s/%%VERSION%%/${version}/g" | sed "s/%%DIC_TYPE%%/${dic_type}/g" > ${temp}/setup.py
+    cd ${temp}
+    python setup.py sdist
+    cp dist/*.tar.gz ${home}/target/
+    cd ${home}
+    rm -r ${temp}
+done
+rm -r target/python/${version}
